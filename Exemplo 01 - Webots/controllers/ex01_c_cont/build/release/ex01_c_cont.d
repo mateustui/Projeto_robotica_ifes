aed78c545bbb406e3c83c771923d660970b1e026
Vinicius de Moura Siqueira <vinic.mourasiqueira@gmail.com>
2021-03-02 23:10:06 -0300

    Correções de limites de contato e parametrização da estrutura como limite geral

--- Exemplo 01 - Webots/controllers/ex01_c_cont/build/release/ex01_c_cont.d
+++ Exemplo 01 - Webots/controllers/ex01_c_cont/build/release/ex01_c_cont.d
@@ -1,7 +1,7 @@
 build/release/ex01_c_cont.o: ex01_c_cont.c \
- C:\Program\ Files\Webots/include/controller/c/webots/keyboard.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h
