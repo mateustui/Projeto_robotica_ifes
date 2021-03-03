85d11558afb07998b4eba8061b5df9bd82bc9b9f
Vinicius de Moura Siqueira <vinic.mourasiqueira@gmail.com>
2021-03-03 08:34:27 -0300

    Adequações na estrutura do projeto, inclusão da arena editável com extensão .obj, ajustes no console

---
+++ controllers/controlador-principal/build/release/controlador-principal.d
@@ -0,0 +1,7 @@
+build/release/controlador-principal.o: controlador-principal.c \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\vinic\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h
--- controllers/controlador-principal/controlador-principal.c
+++ controllers/controlador-principal/controlador-principal.c
@@ -56,7 +56,7 @@ int main()
 
         int key = wb_keyboard_get_key();
 
-        printf("distance: %.2f\ts1:%.2f  \ts2:%.2f  \ts3:%.2f  \ts4:%.2f  \ts5:%.2f\n", valor, s1, s2, s3, s4, s5);
+        printf("| DISTANCIA: %.2f | \t | S1: %.0f \t S2: %.0f \t S3: %.0f \t S4: %.0f \t S5: %.0f |\n", valor, s1, s2, s3, s4, s5);
 
         if (key == 'I')
         {
